4506b22d069331a68b2d6fcb26b84dd676f59f3e
mohitkadwe19 <mohitpawar530@gmail.com>
2022-12-09 23:29:59 +0530

    📦 NEW: fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+function fizzBuzz(A) {
+
+  let result = [];
+
+  for (let i = 1; i <= A; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+console.log(fizzBuzz(15));
