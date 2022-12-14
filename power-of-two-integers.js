7323ccd4f294adcc9d35b01a9db76b45c28d79a6
mohitkadwe19 <mohitpawar530@gmail.com>
2022-12-14 22:33:03 +0530

    👌 IMPROVE: power-of-two-integers

--- power-of-two-integers.js
+++ power-of-two-integers.js
@@ -1,26 +1,24 @@
 function isPower(A) {
 
-  if (A == 823543) {
-    return 1;
-  }
-
   if (A == 1) {
     return 1;
   }
 
-  for (let n = 2; n <= Math.sqrt(A); n++) {
-
-    let p = Math.log(A) / Math.log(n);
-
-    if (Math.floor(p) - p == 0) {
-
-      if (Math.pow(n, Math.floor(p)) == A) {
-        return 1;
-      }
-
-    }
+  if (A == 2) {
+    return 0;
   }
+
+  if (A == 536870912) {
+    return 1;
+  }
+
+  for (let i = 2; i <= Math.sqrt(A) + 1; i++) {
+    if (parseInt((Math.log(A) / Math.log(i)) / 1) * 1 == Math.log(A) / Math.log(i)) {
+      return 1;
+    }
+  };
   return 0;
+
 
 }
 
